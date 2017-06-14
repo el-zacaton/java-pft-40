209a1ba950ae08920ce1a7c2c92fdaaa91182e99
El Zacaton <el.zacaton@gmail.com>
2017-06-14 22:13:03 +0300

    getGroupCount added correctly

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,6 +1,5 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -12,7 +11,6 @@ public class ContactModificationTests extends TestBase {
     @Test
     public void testContactModification() {
         app.getNavigationHelper().gotoHomePage();
-        int before = app.getGroupHelper().getGroupCount();
         if (!app.getContactHelper().isThereAContact()) {
             app.getContactHelper().createContact(
                     new ContactData("Bob", "Smith", "Brooklin", "+19175115832", "bob@hotmail.com", "testGroup33"));
@@ -22,7 +20,5 @@ public class ContactModificationTests extends TestBase {
                 new ContactData("Boob", "Smeeth", "Bruklin", "+19176226943", "boob@hotmail.com", null), false);
         app.getContactHelper().submitContactModification();
         app.getContactHelper().returnToHomePage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before);
     }
 }
